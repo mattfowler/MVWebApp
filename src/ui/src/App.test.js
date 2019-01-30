1dc5b55c41581c36b027b853845ae4022492ae59
matthewfowler <matt.fow@gmail.com>
2019-01-30 18:49:58 -0500

    Fix test

--- src/ui/src/App.test.js
+++ src/ui/src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {HashRouter} from "react-router-dom";
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    const div = document.createElement('div');
+    ReactDOM.render(<HashRouter><App/></HashRouter>, div);
+    ReactDOM.unmountComponentAtNode(div);
 });
