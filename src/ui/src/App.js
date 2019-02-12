f28315ac98b0d311b390a0e01c6636f81d697c4a
matthewfowler <matt.fow@gmail.com>
2019-02-11 20:21:48 -0500

    Fix name

--- src/ui/src/App.js
+++ src/ui/src/App.js
@@ -35,7 +35,7 @@ class App extends Component {
         return (
             <div className='h-100'>
                 <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-                    <div className="navbar-brand">MVWebapp</div>
+                    <div className="navbar-brand">MVWebApp</div>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mr-auto">
                             <li className="nav-item active">
